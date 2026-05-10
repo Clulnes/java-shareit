045090a3fb703467e6f1e9ff71166d70c249190f
Clulnes <art.yusupov2003@yandex.ru>
2026-05-10 23:36:39 +0300

    Финальный вариант ТЗ-16

--- server/src/test/java/ru/practicum/shareit/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/BookingControllerTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.booking.BookingController;
 import ru.practicum.shareit.booking.BookingService;
+import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -18,6 +21,7 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = BookingController.class)
@@ -42,5 +46,35 @@ class BookingControllerTest {
     void getAllByBooker() throws Exception {
         when(bookingService.getAllByBooker(anyLong(), any())).thenReturn(List.of(new BookingResponseDto()));
         mvc.perform(get("/bookings?state=ALL").header("X-Sharer-User-Id", 1)).andExpect(status().isOk());
+    }
+
+    @Test
+    void getAllByOwner() throws Exception {
+        when(bookingService.getAllByOwner(anyLong(), any())).thenReturn(List.of(new BookingResponseDto()));
+
+        mvc.perform(get("/bookings/owner?state=ALL&from=0&size=10")
+                        .header("X-Sharer-User-Id", 1L))
+                        .andExpect(status().isOk());
+    }
+
+    @Test
+    void createBooking_Valid_Returns200() throws Exception {
+        BookItemRequestDto dto = new BookItemRequestDto(1L, LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2));
+        when(bookingService.createBooking(anyLong(), any())).thenReturn(new BookingResponseDto());
+
+        mvc.perform(post("/bookings")
+                        .header("X-Sharer-User-Id", 1L)
+                        .content(mapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk());
+    }
+
+    @Test
+    void getBooking_Valid_Returns200() throws Exception {
+        when(bookingService.getBookingById(anyLong(), anyLong())).thenReturn(new BookingResponseDto());
+
+        mvc.perform(get("/bookings/1")
+                        .header("X-Sharer-User-Id", 1L))
+                        .andExpect(status().isOk());
     }
 }
