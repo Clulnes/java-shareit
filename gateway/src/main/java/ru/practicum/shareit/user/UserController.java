355a91cf51bb153815691c1d78de30fa6627eeea
Clulnes <art.yusupov2003@yandex.ru>
2026-05-10 14:49:32 +0300

    Финальный вариант ТЗ-16(поправил импорты)

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user;
 
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
