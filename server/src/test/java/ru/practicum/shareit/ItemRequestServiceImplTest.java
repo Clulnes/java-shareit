70a97b89b01a8a1ed2a3ce987b40e9bc604b0764
Clulnes <art.yusupov2003@yandex.ru>
2026-05-10 23:06:58 +0300

    Финальный вариант ТЗ-16(снова добавил немного тестов)

--- server/src/test/java/ru/practicum/shareit/ItemRequestServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/ItemRequestServiceImplTest.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
+import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.ItemRepository;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.request.ItemRequestRepository;
 import ru.practicum.shareit.request.ItemRequestServiceImpl;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestResponseDto;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 
@@ -19,9 +23,11 @@ import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,5 +63,35 @@ class ItemRequestServiceImplTest {
         when(itemRepository.findAllByRequest_IdIn(anyList())).thenReturn(List.of());
 
         assertEquals(1, requestService.getUserRequests(1L).size());
+    }
+
+    @Test
+    void getUserRequests_UserNotFound_ThrowsNotFound() {
+        when(userRepository.existsById(1L)).thenReturn(false);
+        assertThrows(NotFoundException.class, () -> requestService.getUserRequests(1L));
+    }
+
+    @Test
+    void getAllRequests_Valid_ReturnsList() {
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(itemRequestRepository.findAllByRequestor_IdNot(eq(1L), any(Sort.class))).thenReturn(List.of(request));
+        when(itemRepository.findAllByRequest_IdIn(anyList())).thenReturn(List.of());
+
+        List<ItemRequestResponseDto> result = requestService.getAllRequests(1L);
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void getAllRequests_UserNotFound_ThrowsNotFound() {
+        when(userRepository.existsById(1L)).thenReturn(false);
+        assertThrows(NotFoundException.class, () -> requestService.getAllRequests(1L));
+    }
+
+    @Test
+    void getRequestById_RequestNotFound_ThrowsNotFound() {
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(itemRequestRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> requestService.getRequestById(1L, 1L));
     }
 }
